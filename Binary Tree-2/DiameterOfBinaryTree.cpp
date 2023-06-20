4c9c657a6f8ea67b1e375f7be1c966ad17eddc63
Shivam Mishra <shivam552mishra@gmail.com>
2023-06-20 07:50:04 +0530

    new tree file added

---
+++ Binary Tree-2/DiameterOfBinaryTree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int maxDia(Node* root,int& maxi){
+        if(root == NULL)
+        return 0;
+        
+        int lh = maxDia(root->left,maxi);
+        int rh = maxDia(root->right,maxi);
+        maxi = max(maxi,lh+rh+1);
+        
+        return 1 + max(lh,rh);
+    }
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root) {
+        int maxi = 0;
+        int height = maxDia(root,maxi);
+        //we are only concerned with maxi i.e. max dia
+        return maxi;
+    }
+    
