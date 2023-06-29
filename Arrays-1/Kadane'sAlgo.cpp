471ff9cd0c7119f2b3a9677a0fd870d8755bdd90
Shivam Mishra <shivam552mishra@gmail.com>
2023-06-29 20:57:21 +0530

    new array file added

---
+++ Arrays-1/Kadane'sAlgo.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+       int currsum = 0,maxsum = INT_MIN;
+        
+          for(int i=0;i<nums.size();i++){
+              
+              currsum += nums[i];
+              
+              if(currsum > maxsum)
+              maxsum = currsum;
+              
+              if(currsum < 0)
+              currsum = 0;
+              
+          }
+    
+        return maxsum;
+    
+    }
+};
