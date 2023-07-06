24caa7251e451d6f25ed2f40e7f4d63a23d53af9
Shivam Mishra <shivam552mishra@gmail.com>
2023-07-07 00:36:22 +0530

    new bs file added

---
+++ BinarySearch/NthRoot.cpp
@@ -0,0 +1,40 @@
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        int s=0;
+
+        int e= x;
+
+        long long int mid= s+(e-s)/2;
+
+        long long int ans=-1;
+
+        while(s<=e){
+
+            long long square= mid*mid;
+
+            if(square==x){
+
+                return mid;
+
+            }
+            if(square<x){
+
+                ans= mid;
+
+                s= mid+1;
+            }
+            else{
+
+                e=mid-1;
+
+            }
+            mid= s+(e-s)/2;
+
+        }
+
+        return ans;
+
+}
+};
