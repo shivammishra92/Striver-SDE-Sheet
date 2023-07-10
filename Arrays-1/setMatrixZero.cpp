6b2d3c1d769781422edea42296824edc209a1ec8
Shivam Mishra <shivam552mishra@gmail.com>
2023-07-11 00:22:46 +0530

    new array file added

---
+++ Arrays-1/setMatrixZero.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+
+       set<int>r;
+       set<int>c;
+       for(int i=0;i<matrix.size();i++){
+           for(int j=0;j<matrix[0].size();j++){
+               if(matrix[i][j] == 0){
+                   r.insert(i);
+                   c.insert(j);
+               }
+           }
+       }
+
+       for(int i=0;i<matrix.size();i++){
+           if(r.find(i) != r.end()){
+           for(int j=0;j<matrix[0].size();j++){
+               matrix[i][j] = 0;
+           }
+         }
+       }
+
+       for(int i=0;i<matrix[0].size();i++){
+           if(c.find(i) != c.end()){
+           for(int j=0;j<matrix.size();j++){
+               matrix[j][i] = 0;
+           }
+         }
+       }
+       
+         
+    }
+};
